f1d2448ba646276bfc8a48d14baf202b3ae4ae70
AImenes <aimenes@icloud.com>
2023-04-09 22:01:58 +0200

    Added a max rewriting parameter

--- engine/unify.py
+++ engine/unify.py
@@ -18,7 +18,7 @@ def unifiable(pair):
         if (g1.var1.bound and g2.var1.bound and g1 != g2):
             return False
 
-    #If they are roles (doesnt need to check g2 as then first if-statement would have triggered)
+    #If they are roles
     if isinstance(g1, AtomRole):
 
         # If the variable 1 position in atom g1 and g2 are bound, but not the same variable
@@ -28,6 +28,15 @@ def unifiable(pair):
         # If the variable 2 position in atom g1 and g2 are bound, but not the same variable
         if (g1.var2.bound and g2.var2.bound) and (g1.var2.original_entry_name != g2.var2.original_entry_name):
             return False
+        
+        # Return false if r1(x,y),r1(z,x), because it would reduce do r1(x,x)
+        if (g1.var1.bound and g1.var2.unbound) and (g2.var2.bound and g2.var1.unbound) and (g1.var1.original_entry_name == g2.var2.original_entry_name):
+            return False
+        
+        # Return false if r1(y,x),r1(x,z), because it would reduce do r1(x,x)
+        if (g1.var2.bound and g1.var1.unbound) and (g2.var1.bound and g2.var2.unbound) and (g1.var2.original_entry_name == g2.var1.original_entry_name):
+            return False
+
 
     #It non of the previous steps are triggered, the atoms are unifiable.
     return True
